ec12d8e22ffde61f179286e80f672b68e4bc3383
Yosef <your@email.com>
2026-03-10 13:32:42 +0000

    finnish 05

---
+++ 05/ex1.py
@@ -0,0 +1,2 @@
+def multiply(first_number: int, second_number: int) -> int:
+    return first_number * second_number
---
+++ 05/ex3.py
@@ -0,0 +1,5 @@
+def factorial(number):
+    result = 1
+    for i in range(1, number + 1):
+        result *= i
+    return result
