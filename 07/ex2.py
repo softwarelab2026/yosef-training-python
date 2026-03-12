b68d80b644ff90f5288cc8f31ee7f6f4159fb00b
Yosef <your@email.com>
2026-03-12 14:30:12 +0000

    fix ex2.py

--- 07/ex2.py
+++ 07/ex2.py
@@ -10,8 +10,8 @@ def sum_number(number: int) -> int:
 
 def number_details(number: int) -> str:
     string_number = str(number)
-    return f"the number is: {number} " \
-       f"the digits are: {string_number[0]}, {string_number[1]}, {string_number[2]}, {string_number[3]}, {string_number[4]} " \
+    return f"the number is: {number}\n" \
+       f"{','.join(string_number)}\n" \
        f"the sum of the number is: {sum_number(number)}"
 
 
